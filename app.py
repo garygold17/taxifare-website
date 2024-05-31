fd61b7e9806df3c1547d0380ebd2f9bbcee0e04e
Gary Goldblatt <goldblatt.gary@gmail.com>
2024-05-31 15:12:06 +0200

    save

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 import requests
 from datetime import datetime
 
+
 # Set up parameters for the app
-title = st.title("Calculating taxi fare in New York City!")
+title = st.title("Calculating taxi fare in new york city!")
 
 st.image('image.png')
 
@@ -20,30 +21,30 @@ dropoff_latitude = st.slider('Dropoff Latitude', min_value=-90.0, max_value=90.0
 
 passenger_count = st.number_input('Passenger Count',value=0)
 
+
+
 url = 'https://taxifare.lewagon.ai/predict'
 
+#if url == 'https://taxifare.lewagon.ai/predict'
+
+    #st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+
+# Let's build a dictionary containing the parameters for our API...
+
+api_params = {
+    'pickup_datetime': pickup_datetime.strftime('%Y-%m-%d %H:%M:%S'),
+    'pickup_longitude': pickup_longitude,
+    'pickup_latitude': pickup_latitude,
+    'dropoff_longitude': dropoff_longitude,
+    'dropoff_latitude': dropoff_latitude,
+    'passenger_count': passenger_count
+}
 # Let's call our API using the `requests` package...
 if st.button('Get Fare Prediction'):
-    # Let's build a dictionary containing the parameters for our API...
-    api_params = {
-        'pickup_datetime': pickup_datetime.strftime('%Y-%m-%d %H:%M:%S'),
-        'pickup_longitude': pickup_longitude,
-        'pickup_latitude': pickup_latitude,
-        'dropoff_longitude': dropoff_longitude,
-        'dropoff_latitude': dropoff_latitude,
-        'passenger_count': passenger_count
-    }
-    # Let's retrieve the prediction from the **JSON** returned by the API...
-if st.button('Get Fare Prediction'):
-
     response = requests.get(url, params=api_params)
-
+    # Let's retrieve the prediction from the **JSON** returned by the API...
     if response.status_code == 200:
-
         prediction = response.json().get('fare', 'Error: No fare returned')
-
         st.write(f'Estimated Fare: ${prediction}')
-
     else:
-
         st.write('Error: Failed to retrieve prediction')
